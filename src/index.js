fac0e3b86d7e58b0fa56d10047b37fcc5fbeff89
keiroqq <artem.nenashev04@gmail.com>
2024-12-08 02:05:45 +0400

    feat: LAB4-3 Добавлены ранее созданные страницы в проект

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import './styles/menuStyle.css'
+import './styles/styles.css'
+import './styles/tableStyles.css'
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,73 @@
-const path = require('path'); // Импортируем модуль "path" для работы с путями файлов
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path"); // Импортируем модуль "path" для работы с путями файлов
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js', // Точка входа для сборки проекта
-	output: {
-		filename: 'bundle.js', // Имя выходного файла сборки
-		path: path.resolve(__dirname, 'dist'), // Путь для выходного файла сборки
-	},
-	
-	module: {
-		rules: [
-		{
-			test: /\.css$/, // Регулярное выражение для обработки файлов с расширением .css
-			use: ['style-loader', 'css-loader'], // Загрузчики, используемые для обработки CSS-файлов
-		},
-		],
-	},
-	
-	plugins: [
+  entry: "./src/index.js", // Точка входа для сборки проекта
+
+  output: {
+    filename: "bundle.js", // Имя выходного файла сборки
+    path: path.resolve(__dirname, "dist"), // Путь для выходного файла сборки
+  },
+
+  devServer: {
+    liveReload: true,
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    compress: true,
+    port: 3000,
+    open: true,
+    liveReload: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+
+  plugins: [
 		new HtmlWebpackPlugin({
-		template: './src/index.html',
-		inject: true,
-		chunks: ['index'],
-		filename: 'index.html'
+			template: './src/index.html',
+			inject: true,
+			chunks: ['index'],
+			filename: 'index.html'
 		}),
+		new HtmlWebpackPlugin({
+			template: './src/tasks.html',
+			inject: true,
+			chunks: ['index'],
+			filename: 'tasks.html'
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/error.html',
+			inject: true,
+			chunks: ['index'],
+			filename: 'error.html'
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/about.html',
+			inject: true,
+			chunks: ['index'],
+			filename: 'about.html'
+		}),
+		new CopyWebpackPlugin({
+			patterns: [
+			{ from: './src/scripts/jquery-3.7.1.slim.min.js', to: './scripts/jquery-3.7.1.slim.min.js' }, // Копируем jQuery
+			{ from: './src/scripts/menu.js', to: './scripts/menu.js' }, // Копируем menu.js
+			{ from: './src/scripts/modal.js', to: './scripts/modal.js' }, // Копируем modal.js
+			{ from: './src/images/', to: './images/' } // Копируем все изображения из папки src/images
+      ],
+    }),
 	],
-	
-	devServer: {
-		static: {
-			directory: path.join(__dirname, 'dist'), // Каталог для статики
-		},
-		open: true, // Автоматически открывать браузер
-	},
-	
-	mode: 'development', // Режим сборки
+
+  mode: "development", // Режим сборки
 };
